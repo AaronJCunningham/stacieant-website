d83bd62cd5504f3573d1a92a2fd76430ee0b18c2
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2021-06-10 16:31:49 +0200

    seo added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,42 @@
 import * as React from "react";
-import { Link as GatsbyLink } from "gatsby";
-import { Container, Stack, Box, Heading, Text, Link } from "@chakra-ui/react";
+import { useStaticQuery, graphql, Link as GatsbyLink } from "gatsby";
+import { Container, Stack, Box, Heading, Link } from "@chakra-ui/react";
 
 const HomePage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allWpPost {
+        nodes {
+          id
+          title
+          excerpt
+          uri
+        }
+      }
+    }
+  `);
+
+  const { allWpPost } = data;
+
   return (
     <Container maxW="xl" centerContent>
       <Heading as="h1" m="5">
         #100DaysofGatsby
       </Heading>
       <Stack spacing={8}>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as="h2">Hello World</Heading>
-          <Text mt="4">
-            <span dangerouslySetInnerHTML={{ __html: "<p>example text</p>" }} />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read More{" "}
-          </Link>
-        </Box>
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Heading as="h2">Hello World</Heading>
-          <Text mt="4">
-            <span dangerouslySetInnerHTML={{ __html: "<p>example text</p>" }} />
-          </Text>
-          <Link as={GatsbyLink} to="#">
-            Read More{" "}
-          </Link>
-        </Box>
+        {allWpPost.nodes.map(({ id, title, excerpt, uri }) => (
+          <Box key={id} p={5} shadow="md" borderWidth="1px">
+            <Heading as="h2">{title}</Heading>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: excerpt,
+              }}
+            ></div>
+            <Link as={GatsbyLink} to={uri}>
+              Read More{" "}
+            </Link>
+          </Box>
+        ))}
       </Stack>
     </Container>
   );
