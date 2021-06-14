b8bd94fb6ad62a39067dadadaff32b58dc7999b5
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2021-06-14 10:14:19 +0200

    glitch

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
       resolve: "gatsby-source-wordpress",
       options: {
         url: "http://stacieant.local/graphql",
-        ENABLE_GATSBY_REFRESH_ENDPOINT: true,
       },
     },
     {
